cf9a666e068ad54fc8fd751aac261248ae26a432
TriGonostylops <martonbalazsi@gmail.com>
2026-02-12 08:20:14 +0100

    feat: undo redundant preproc package

--- preprocess_full_resolution/src/config.py
+++ preprocess_full_resolution/src/config.py
@@ -6,15 +6,14 @@ RESOURCES_DIR = BASE_DIR / "resources"
 VIDEO_PATH = RESOURCES_DIR / "01.59.10-02.03.13[M][0@0][0].mp4"
 XML_PATH = RESOURCES_DIR / "annotations.xml"
 
-OUTPUT_DIR = BASE_DIR / "dataset"
-INTERIM_DIR = OUTPUT_DIR / "interim"
-FILTERED_DIR = OUTPUT_DIR / "interim_filtered"
-PROCESSED_DIR = OUTPUT_DIR / "processed"
+OUTPUT_DIR = BASE_DIR / "b5-dataset"
+INTERIM_DIR = OUTPUT_DIR / "b5-interim"
+FILTERED_DIR = OUTPUT_DIR / "b5-interim_filtered"
+PROCESSED_DIR = OUTPUT_DIR / "b5-processed"
 
 IOU_THRESHOLD = 0.8
-TARGET_SIZE = 1024
+TARGET_SIZE = 640
 
-# Augmentation Settings
 AUGMENT_MULTIPLIER = 1  # Number of augmented copies per original image
 AUGMENTATION_SEED = 42
 
